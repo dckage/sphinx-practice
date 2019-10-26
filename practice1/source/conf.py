34167bbe18913640073e1b399c9e09db93aeaf5a
D.C.Kage <d.c.kage@d.c.kage>
2019-10-26 15:39:54 +0900

    Adopt redcloud theme

--- practice1/source/conf.py
+++ practice1/source/conf.py
@@ -1,3 +1,4 @@
+import cloud_sptheme as csp
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -51,7 +52,10 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'redcloud'
+
+# set the theme path to point to cloud's theme data
+html_theme_path = [csp.get_theme_dir()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
